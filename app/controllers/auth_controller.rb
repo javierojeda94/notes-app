3c9b71155b6bfdd94174f6fb76511bc4d3304258
Javier Ojeda <ojeda.javier94@gmail.com>
2017-06-29 20:04:46 -0500

    add signup functionallity

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,14 +5,40 @@ class AuthController < ApplicationController
   def login
     authentication = AuthenticateUser.call(params[:email], params[:password])
     if authentication.success?
-      render json: { auth_token: authentication.result }
+      response = {
+        auth_token: authentication.result,
+        status: :ok
+      }
     else
-      render json: { error: authentication.errors }, status: :unauthorized
+      response = {
+        error: authentication.errors,
+        status: :unauthorized
+      }
     end
+    render json: response, status: response[:status]
   end
 
   def signup
+    user = User.new(user_params)
+    if user.save
 
+      response = {
+        message: 'User was successfully created in the database! Now you can login!',
+        status: :ok
+      }
+    else
+      response = {
+        error: user.errors.to_a.join(','),
+        status: :unprocesable_entity
+      }
+    end
+    render json: response, status: response[:status]
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 
 end
